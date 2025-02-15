0e63bf8acb6f4baaa919cd0974720945d47cca00
Guilherme Carius <gcarius@Guilhermes-MacBook-Pro-2.local>
2025-02-15 13:33:33 -0700

    wip

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 view = Blueprint("view", __name__)
 
+
 @view.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
