79f43b5d50197a069cbe9ac4ff8140205d934384
Guilherme Carius <gcarius@Guilhermes-MacBook-Pro-2.local>
2025-02-15 13:13:56 -0700

    flask wip

--- website/__init__.py
+++ website/__init__.py
@@ -5,8 +5,7 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "tattlab"
     
-    @app.route("/")
-    def home():
-        return "<h1>Hello World!!!<h1>"
+    from .views import view
+    app.register_blueprint(view, url_prefix="/")
 
     return app
