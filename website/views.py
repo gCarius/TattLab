31c94234ae9606533feeaac14d5e8475be4d37e0
Guilherme Carius <gcarius@Guilhermes-MacBook-Pro-2.local>
2025-02-15 13:43:31 -0700

    wip

--- website/views.py
+++ website/views.py
@@ -6,3 +6,7 @@ view = Blueprint("view", __name__)
 @view.route("/")
 def home():
     return render_template("home.html")
+
+@view.route("/test")
+def test():
+    return render_template("test.html")
